3d446f99dbe5a9d4336a21449506b3eee6caf262
Tomé Eduardo <l.tome.eduardo@accenture.com>
2022-10-03 20:17:57 -0300

    Conexão estabelecida com sucesso no SQL Server - Classe: TesteSqlServer

--- app/app-java/src/main/java/classes/app/TesteSqlServer.java
+++ app/app-java/src/main/java/classes/app/TesteSqlServer.java
@@ -9,20 +9,15 @@ public class TesteSqlServer {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
     public static void main(String[] args) {
-        String connectionUrl =
-                "jdbc:sqlserver://yourserver.database.windows.net:1433;"
-                        + "database=datasentry;"
-                        + "user=SA;"
-                        + "password=#Gfgrupo1;"
-                        + "encrypt=true;"
-                        + "trustServerCertificate=false;"
-                        + "loginTimeout=30;";
+        String connectionUrl
+                = "jdbc:sqlserver://;serverName=localhost;encrypt=true;trustServerCertificate=true";
+        String user = "sa";
+        String password = "#Gfgrupo1";
+        try {
+            Connection connection = DriverManager.getConnection(connectionUrl, user, password);
+            System.out.println(connection);
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-            System.out.println("SUCESSO");
-            // Code here.  
-        } // Handle any errors that may have occurred.
-        catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
