540ad2f172e86d50a60295c42cae8213be56690c
Tomé Eduardo <l.tome.eduardo@accenture.com>
2022-10-03 21:28:19 -0300

    Git pull branch Matheus

--- app/app-java/src/main/java/classes/app/TesteSqlServer.java
+++ app/app-java/src/main/java/classes/app/TesteSqlServer.java
@@ -1,10 +1,14 @@
 package classes.app;
 
+import com.github.britooo.looca.api.core.Looca;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TesteSqlServer {
+    
+            Looca looca = new Looca();
+        String dadosFabricante = looca.getSistema().getFabricante();
 
     // Connect to your database.
     // Replace server name, username, and password with your credentials
