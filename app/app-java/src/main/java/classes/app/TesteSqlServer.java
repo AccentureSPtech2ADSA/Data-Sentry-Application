acbac4ea21a613a841809ed045e2d3c5adb8ba5d
leonardotome <leonardo.eduardo@sptech.school>
2022-10-16 17:48:27 -0300

    Conexão com SQL Server Azure feita - Exclusão da classe de teste de insert na table server do pacote getDados

--- app/app-java/src/main/java/classes/app/TesteSqlServer.java
+++ app/app-java/src/main/java/classes/app/TesteSqlServer.java
@@ -15,15 +15,21 @@ public class TesteSqlServer {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
     public static void main(String[] args) {
-        String connectionUrl
-                = "jdbc:sqlserver://;serverName=localhost;databaseName=datasentry;encrypt=true;trustServerCertificate=true";
-        String user = "sa";
-        String password = "#Gfgrupo1";
+        // CONEXÃO SQL SERVER - DBEAVER        
+//        String connectionUrl
+//                = "jdbc:sqlserver://;serverName=localhost;databaseName=datasentry;encrypt=true;trustServerCertificate=true";
+//        String user = "sa";
+//        String password = "#Gfgrupo1";
+
+        // CONEXÃO SQL SERVER - AZURE
+String connectionUrl
+        = "jdbc:sqlserver://datasentry.database.windows.net:1433;database=datasentry;user=datasentry@datasentry;password=#Gfgrupo1;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+
         try {
-            Connection connection = DriverManager.getConnection(connectionUrl, user, password);
+            Connection connection = DriverManager.getConnection(connectionUrl);
             
             Statement stm = connection.createStatement();
-            stm.executeUpdate("INSERT INTO contactPhoneHospital VALUES ('1234567890123', '1', NULL, NULL)");
+            stm.executeUpdate("INSERT INTO Server VALUES ('teste', '1', 'linux', '1', NULL, NULL)");
             
             System.out.println(connection);
 
